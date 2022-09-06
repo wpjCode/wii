408d7e451c261ce266cebb2069fabbb55381f290
wpj <Ww..11>
2022-09-06 09:22:26 +0800

    some format code

--- src/generators/crud/Generator.php
+++ src/generators/crud/Generator.php
@@ -997,7 +997,8 @@ EOT;
         $row = [
             '下面是【后台】页面展示连接：',
             '{space}// [' . $this->expName . ']列表 页面',
-            "{space}\$urlPre . '" . $route . "/index-page',",
+            "{space}\$urlPre . '" . lcfirst($baseName) . ".html' => \$routePre . '" .
+            $route . "/index-page',",
             '{space}// [' . $this->expName . ']添加 页面',
             "{space}\$urlPre . '" . lcfirst($baseName) . "Create.html' => \$routePre . '" .
             $route . "/create-page',",
